8b4eb1598d82a1846e7ca3aa5e541e2fce1f4b58
ditaagn <131266806+ditaagn@users.noreply.github.com>
2023-05-11 13:50:32 +0700

    Update Main.java

--- Soal 1/src/Main.java
+++ Soal 1/src/Main.java
@@ -1,11 +1,16 @@
-public class Main{
-public static void main(String[] args) {
-        Mahasiswa mhs1 = new Mahasiswa("Taylor Swift", 19, "Computer Science"); //Objek adalah sesuatu yang dihasilkan oleh class. mhs1 adalah objek
-        mhs1.setGpaAndSemester(10.5, 3);
-        mhs1.calculateIpk();
-        System.out.println("");
+public class Main {
+        public static void main(String[] args) {
+                Mahasiswa mhs1 = new Mahasiswa("Taylor Swift", 19, "Computer Science"); // Objek adalah sesuatu yang
+                // dihasilkan oleh class. mhs1
+                // adalah objek
+                mhs1.setGpaAndSemester(10.5, 3);
+                mhs1.calculateIpk(3);
 
-        Mahasiswa mhs2 = new Mahasiswa("Lee Jeno", 20, "Electrical Engineering", 24.0, 6); //mhs2 juga merupakan objek
-        mhs2.calculateIpk(6);
+                System.out.println("");
+
+                Mahasiswa mhs2 = new Mahasiswa("Lee Jeno", 20, "Electrical Engineering", 22.0, 6); // mhs2 juga
+                // merupakan objek
+                mhs2.calculateIpk(-3.5);
+
         }
 }
