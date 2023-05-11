2f59b780a04b18edcf24f173d5b8694eb773e1f1
ditaagn <131266806+ditaagn@users.noreply.github.com>
2023-05-11 13:50:12 +0700

    Update Mahasiswa.java

--- Soal 1/src/Mahasiswa.java
+++ Soal 1/src/Mahasiswa.java
@@ -1,11 +1,14 @@
-public class Mahasiswa { //Class adalah template atau blueprint dari objek. Mahasiswa adalah class
+public class Mahasiswa { // Class adalah template atau blueprint dari objek. Mahasiswa adalah class
     private String name;
     private int age;
     private String major;
     private double totalGpa;
     private int totalSemester;
-    //Constructor adalah sebuah metode yang digunakan untuk menginisialisasi objek yang baru dibuat
-    //Contoh constructor
+    private double ipk;
+
+    // Constructor adalah sebuah metode yang digunakan untuk menginisialisasi objek
+    // yang baru dibuat
+    // Contoh constructor
     public Mahasiswa(String name, int age, String major) {
         this.name = name;
         this.age = age;
@@ -19,8 +22,10 @@ public class Mahasiswa { //Class adalah template atau blueprint dari objek. Maha
         this.totalGpa = totalGpa;
         this.totalSemester = totalSemester;
     }
-    //Method setter getter adalah metode yang digunakan untuk mengakses dan mengubah nilai variabel dalam sebuah class
-    //Contoh method setter getter
+
+    // Method setter getter adalah metode yang digunakan untuk mengakses dan
+    // mengubah nilai variabel dalam sebuah class
+    // Contoh method setter getter
     public void setName(String name) {
         this.name = name;
     }
@@ -34,25 +39,42 @@ public class Mahasiswa { //Class adalah template atau blueprint dari objek. Maha
     }
 
     public void setGpaAndSemester(double totalGpa, int totalSemester) {
-        this.totalGpa = totalGpa;
-        this.totalSemester = totalSemester;
-    }
-    //Contoh method overloading
-    //Overloading adalah keadaan dimana satu class memiliki method dengan nama yang sama namun parameter yang berbeda
-    public void calculateIpk() {
-        double ipk = totalGpa / totalSemester;
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Major: " + major);
-        System.out.println("IPK: " + ipk);
+        if (this.totalGpa >= 0) {
+            this.totalGpa = totalGpa;
+        } else {
+            System.out.println("Input invalid");
+        }
+        if (this.totalSemester >= 0) {
+            this.totalSemester = totalSemester;
+        } else {
+            System.out.println("Input invalid");
+        }
     }
 
+    // Contoh method overloading
+    // Overloading adalah keadaan dimana satu class memiliki method dengan nama yang
+    // sama namun parameter yang berbeda
+
     public void calculateIpk(int totalSemester) {
-        double ipk = totalGpa / totalSemester;
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Major: " + major);
-        System.out.println("IPK: " + ipk);
-        System.out.println("Total Semester : " + totalSemester);
+        if (totalSemester >= 0) {
+            this.ipk = this.totalGpa / this.totalSemester;
+            System.out.println(this.toString());
+        } else {
+            System.out.println("Input tidak valid");
+        }
     }
+
+    public void calculateIpk(double totalGpa) {
+        if (totalGpa >= 0) {
+            this.ipk = this.totalGpa / this.totalSemester;
+            System.out.println(this.toString());
+        } else {
+            System.out.println("Input tidak valid");
+        }
+    }
+
+    public String toString() {
+        return "Name  : " + this.name + " | Age  : " + this.age + " | Major : " + this.major + " |  IPK : " + this.ipk;
+    }
+
 }
